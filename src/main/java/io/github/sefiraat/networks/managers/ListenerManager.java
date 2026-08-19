ccb0eb5d55d13998675b4fc32c5a5a7195b74184
wickidcow <jaywago@gmail.com>
2026-08-18 22:24:12 -0400

    Show stored items on Quantum Storage

--- src/main/java/io/github/sefiraat/networks/managers/ListenerManager.java
+++ src/main/java/io/github/sefiraat/networks/managers/ListenerManager.java
@@ -5,12 +5,14 @@ import com.balugaq.netex.core.listeners.JEGCompatibleListener;
 import io.github.sefiraat.networks.Networks;
 import io.github.sefiraat.networks.listeners.ExplosiveToolListener;
 import io.github.sefiraat.networks.listeners.SyncListener;
+import io.github.sefiraat.networks.utils.QuantumStorageDisplayManager;
 import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
 public class ListenerManager {
 
     public ListenerManager() {
+        QuantumStorageDisplayManager.initialize();
         addListener(new ExplosiveToolListener());
         addListener(new SyncListener());
         if (Networks.getSupportedPluginManager().isJustEnoughGuide()) {
