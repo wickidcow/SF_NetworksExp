b05c73237f48e4ca68027f8091be9cb818fd0f45
wickidcow <jaywago@gmail.com>
2026-09-04 07:55:00 -0400

    Register network integrity protection

--- src/main/java/io/github/sefiraat/networks/managers/ListenerManager.java
+++ src/main/java/io/github/sefiraat/networks/managers/ListenerManager.java
@@ -4,6 +4,7 @@ import com.balugaq.netex.core.listeners.HangingBlockInteractListener;
 import com.balugaq.netex.core.listeners.JEGCompatibleListener;
 import io.github.sefiraat.networks.Networks;
 import io.github.sefiraat.networks.listeners.ExplosiveToolListener;
+import io.github.sefiraat.networks.listeners.NetworkIntegrityListener;
 import io.github.sefiraat.networks.listeners.SyncListener;
 import io.github.sefiraat.networks.utils.QuantumStorageDisplayManager;
 import org.bukkit.event.Listener;
@@ -14,6 +15,7 @@ public class ListenerManager {
     public ListenerManager() {
         QuantumStorageDisplayManager.initialize();
         addListener(new ExplosiveToolListener());
+        addListener(new NetworkIntegrityListener());
         addListener(new SyncListener());
         if (Networks.getSupportedPluginManager().isJustEnoughGuide()) {
             // todo: remove and deprecate
