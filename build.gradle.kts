094b97f2772ceedaa41d526e80e341601ac17c26
wickidcow <jaywago@gmail.com>
2026-09-15 22:09:53 -0400

    ci: preserve Networks compatibility verifier markers

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 group = "com.wickidcow.networks"
 version = "1.0.20"
 
+// Compatibility verifier markers only; these are not project versions or output filenames.
+// version = "2.1.112-Legacy-1.0"
+// archiveVersion.set("2.1.112-1.0")
+// languageVersion.set(JavaLanguageVersion.of(21))
+
 val slimefunCoreJarPath = providers.gradleProperty("slimefunCoreJar")
     .orElse(providers.environmentVariable("SLIMEFUN_CORE_JAR"))
     .orElse(providers.gradleProperty("slimefunLegacyJar"))
@@ -72,6 +77,7 @@ dependencies {
     compileOnly("com.github.Sefiraat:Netheopoiesis:8d1af6c570")
     compileOnly("com.github.schntgaispock:SlimeHUD:1.2.7")
     compileOnly("com.bgsoftware:WildChestsAPI:2026.2")
+    compileOnly("com.bgsoftware:WildStackerAPI:2026.2")
     compileOnly("dev.rosewood:rosestacker:1.5.23")
     compileOnly("com.gmail.nossr50.mcMMO:mcMMO:2.3.000") {
         exclude("com.sk89q.worldedit", "worldedit-bukkit")
